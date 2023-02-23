1cd64f9e92326536f2c3df16bc9bf8c6085ba6b0
OSCARJOV <96828560+OSCARJOV@users.noreply.github.com>
2023-02-22 19:24:16 -0500

    jwt2

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const { matchedData } = require("express-validator");
+const { validatorRegister } = require("../validators/auth");
 const router = express.Router();
 
 
-router.post("/",);
+router.post("/register", validatorRegister, (req,res) => {
+
+    req = matchedData(req);
+    res.send({data:req})
+});
 
 module.exports = router
